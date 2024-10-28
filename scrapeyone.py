ecd57950a2fb9fdb3baad5eea5bd2aef244ac9fd
Melphisoup <kmatt5445@gmail.com>
2024-10-28 00:41:08 -0400

    Add basic code for scrapeyone

--- scrapeyone.py
+++ scrapeyone.py
@@ -1,8 +1,17 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 
-
-
-options = webdriver.Chromeoptions('--headless')
-driver = webdriver(options=options
+options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+driver = webdriver.Chrome(options=options)
+target_url="https://www.scrapethissite.com/pages/simple/"
+driver.get(target_url)
+revealed = driver.find_elements(By.CLASS_NAME, "country-name")
+wait = WebDriverWait(driver, timeout=2)
+wait.until(lambda d : revealed)
+del revealed[1]
+for x in revealed:
+    print(x.text)
