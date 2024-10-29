40ab03c130894594d262278f9a3cc11bcfe518d3
Melphisoup <kmatt5445@gmail.com>
2024-10-28 20:31:36 -0400

    Add csv file for storing data Change code for scrapeyone     trying different methods to store and manipulate data

--- scrapeyone.py
+++ scrapeyone.py
@@ -1,3 +1,5 @@
+import csv
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
@@ -9,9 +11,12 @@ options.add_argument('--headless')
 driver = webdriver.Chrome(options=options)
 target_url="https://www.scrapethissite.com/pages/simple/"
 driver.get(target_url)
-revealed = driver.find_elements(By.CLASS_NAME, "country-name")
+country = driver.find_elements(By.CLASS_NAME, "country-name")
 wait = WebDriverWait(driver, timeout=2)
-wait.until(lambda d : revealed)
-del revealed[1]
-for x in revealed:
-    print(x.text)
+wait.until(lambda d : country)
+filename = "data.csv"
+
+file = open(filename, 'w', newline='')
+with file:
+    write = csv.writer(file)
+    write.writerows(country.text)
